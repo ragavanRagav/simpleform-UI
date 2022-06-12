fbf36d9bc3cca97d5b73c1a1c04f93bde2e5ef64
Ragavan <ragavan111999@gmail.com>
2022-06-13 00:23:56 +0530

    deployment fix

--- src/pages/ShowDetails/ShowDetails.jsx
+++ src/pages/ShowDetails/ShowDetails.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getDetails } from "../../servicecalls/backend-service-call";
 
 const ShowDetails = () => {
   let { email } = useParams();
+  let navigate = useNavigate();
   const [details, setDetails] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const fetchDetails = async (mail) => {
     setLoading(true);
     const res = await getDetails(mail);
     if (res.status) {
       setDetails(res?.details);
+    } else {
+      setError(res.details);
+      setInterval(() => {
+        navigate("/");
+      }, 1000);
     }
     setLoading(false);
   };
@@ -26,17 +33,24 @@ const ShowDetails = () => {
           <h4>Loading please wait...</h4>
         ) : (
           <>
-            {details?.map((detail) => {
-              return (
-                <div className="text-capitalize border w-75 m-auto my-2 d-flex justify-content-begin flex-column" key={detail.id}>
-                  <h5>name : {detail.name}</h5>
-                  <h5>email : {detail.email}</h5>
-                  <h5>country : {detail.country}</h5>
-                  <h5>noTravelers : {detail.noTravelers}</h5>
-                  <h5>budget : {detail.budget}</h5>
-                </div>
-              );
-            })}
+            {!error ? (
+              details?.map((detail) => {
+                return (
+                  <div
+                    className="text-capitalize border w-75 m-auto my-2 d-flex justify-content-begin flex-column"
+                    key={detail.id}
+                  >
+                    <h5>name : {detail.name}</h5>
+                    <h5>email : {detail.email}</h5>
+                    <h5>country : {detail.country}</h5>
+                    <h5>noTravelers : {detail.noTravelers}</h5>
+                    <h5>budget : {detail.budget}</h5>
+                  </div>
+                );
+              })
+            ) : (
+              <h5 className="text-danger text-capitalize">{error}</h5>
+            )}
           </>
         )}
       </div>
--- src/servicecalls/backend-service-call.js
+++ src/servicecalls/backend-service-call.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const backendUrl = "http://localhost:8080";
+const backendUrl = false ? "http://localhost:8080" : "https://tagalysbackendtask.herokuapp.com";
 
 const saveDetails = async (details) => {
-  console.log("Details => ", details);
   return axios.post(`${backendUrl}/saveDetails`,details).then((resp)=>{
       return resp.data;
   }).catch((err)=>{
@@ -12,7 +11,6 @@ const saveDetails = async (details) => {
   })
 };
 const getDetails = async (email) => {
-  console.log("Details => ", email);
   return axios
     .get(`${backendUrl}/details/${email}`)
     .then((res) => {
